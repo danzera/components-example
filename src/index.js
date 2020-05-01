a14febcc227a20c1e7b5f9e551d98eac75077cc1
Dan Zera <dczera@gmail.com>
2020-05-01 14:31:14 -0700

    factor comments into a separate component and pass details in as props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
+// MODULE IMPORTS
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+// FILE IMPORTS
 import CommentDetail from './CommentDetail';
 
 const App = _ => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail />
-			<CommentDetail />
-			<CommentDetail />
+			<CommentDetail
+				author="Sam"
+				time="Today at 4:45PM"
+				comment="I'm tall."
+				avatar={ faker.image.avatar() }
+			/>
+			<CommentDetail
+				author="Jon"
+				time="Today at 2:00PM"
+				comment="When will baseball start?"
+				avatar={ faker.image.avatar() }
+			/>
+			<CommentDetail
+				author="Ben"
+				time="Yesterday at 8:30PM"
+				comment="Squirrels are the worst."
+				avatar={ faker.image.avatar() }
+			/>
+			<CommentDetail
+				author="Kyle"
+				time="Yesterday at 3:15PM"
+				comment="GO RAIDERS!"
+				avatar={ faker.image.avatar() }
+			/>
 		</div>
 	);
 }
